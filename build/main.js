516fa8dde4a9ca8a1d5486b22ddd72c2acf29212
Gaoyu <13996147982@163.com>
2018-09-18 14:39:45 +0800

    修改ico

--- build/main.js
+++ build/main.js
@@ -397,7 +397,7 @@ module.exports = {
   head: {
     title: 'starter',
     meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: 'Nuxt.js project' }],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/micaiah.ico' }]
   },
   /*
   ** Global CSS
